2cb6005d7bbcfeb8d00e47181a8eecd2460521c7
Christopher Stott <christopherstott@gmail.com>
2015-07-04 18:14:48 -0700

    Initial Commit

---
+++ BBAppTemplate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BBAppTemplate
+//
+//  Created by Chris on 2015-07-04.
+//  Copyright (c) 2015 BB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
